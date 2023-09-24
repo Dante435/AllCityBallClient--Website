d5e25fe57b79500ab402e313483f35218271f0ac
Dante <127224745+Dante435@users.noreply.github.com>
2023-09-24 02:32:29 +0100

    comit

--- src/constants/index.js
+++
@@ -1,7 +0,0 @@
-import images from './images';
-import vid3 from '../assets/vid_3.MOV';
-import vid2 from '../assets/vid_2.MOV';
-import vid1 from '../assets/vid_1.MOV';
-import data from './data';
-
-export { images, vid3, vid2, vid1, data };
